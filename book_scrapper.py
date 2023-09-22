6c752cf3b8697e86510a99f8097efcb82ce457d1
Ahmed Soltani <ahmed.soltani2026@gmail.com>
2023-09-22 22:05:01 +0200

    Scraping

--- book_scrapper.py
+++ book_scrapper.py
@@ -1,4 +1,35 @@
 import sqlite3
 import requests
 from bs4 import BeautifulSoup
-from time import sleep 
+from time import sleep  
+
+def scarpe_books(url):
+	response = requests.get(url)
+	soup = BeautifulSoup(response.text, "html.parser")
+	books = soup.find_all("article") 
+	all_books = []
+	for book in books:
+		book_data = (get_title(book),get_price(book),get_rating(book))
+		all_books.append(book_data)
+		print(all_books)
+
+def get_title(book):
+	return book.find("h3").find("a")["title"]
+
+def get_price(book):
+	price = book.select(".price_color")[0].get_text()
+	return float(price.replace("£","").replace("Â",""))
+def get_rating(book):
+	ratings = {
+	"Zero": 0,
+	"One": 1,
+	"Two": 2,
+	"Three": 3,
+	"Four": 4,
+	"Five": 5
+	}
+	paragraph = book.select(".star-rating")[0]
+	word = paragraph.get_attribute_list("class")[-1]
+	return ratings[word]
+
+scarpe_books("https://books.toscrape.com/catalogue/category/books/history_32/index.html")
